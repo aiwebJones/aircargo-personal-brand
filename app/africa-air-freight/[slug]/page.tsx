2e00a377ff8374440e51c59e008cb9b5bcecb0c3
aiwebJones <globegsa@gmail.com>
2026-07-26 09:10:42 +0800

    Unify EASCargo entity schema references

--- app/africa-air-freight/[slug]/page.tsx
+++ app/africa-air-freight/[slug]/page.tsx
@@ -1645,7 +1645,7 @@ export default function AfricaDestinationPage({ params }: { params: { slug: stri
             '@context': 'https://schema.org',
             '@type': 'Service',
             name: item.title,
-            provider: { '@type': 'Organization', name: 'EASCargo Jones', url: 'https://www.eascargo.com' },
+            provider: { '@type': 'Organization', '@id': 'https://www.eascargo.com/#organization', name: 'EASCargo Jones', url: 'https://www.eascargo.com' },
             areaServed: item.country,
             serviceType: 'Oversized air freight and project cargo routing',
             description: item.description,
