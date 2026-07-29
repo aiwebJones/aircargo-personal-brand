2b3ff2c696358c7c7467e0ed48a15f084a93afe9
aiwebJones <globegsa@gmail.com>
2026-07-29 09:19:38 +0800

    Move AI source under allowed public path: update public/_worker.js

--- public/_worker.js
+++ public/_worker.js
@@ -3,6 +3,8 @@ const PREVIEW_HOST = 'aircargo-personal-brand.pages.dev'
 const APEX_HOST = 'eascargo.com'
 const LEGACY_REDIRECTS = new Map([
   ['/about/', '/'],
+  ['/answer-engine/', '/insights/eascargo-answer-engine-profile/'],
+  ['/answer-engine-profile.json/', '/insights/eascargo-answer-engine-profile.json'],
   ['/insights/africa-oil-gas-equipment-air-freight/', '/insights/nigeria-phc-oil-gas-spares-soncap-nsw-air-freight-clearance/'],
   ['/insights/africa-project-cargo-air-freight-rfq-checklist/', '/insights/oversized-air-freight-quote-checklist/'],
   ['/en/africa-mining-spares-air-freight/', '/en/africa-oversized-air-freight/'],
